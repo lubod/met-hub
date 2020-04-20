ce37f4b03413ce02cf6e091e4f8a342057d5b7c8
Lubo Drobny <lubo.drobny@gmail.com>
2020-04-20 16:42:22 +0200

    db version

--- main.js
+++ main.js
@@ -1,25 +1,53 @@
+const { Pool } = require('pg');
 var express = require('express');
+
 var app = express();
 var fs = require("fs");
-app.use(express.static(__dirname));  
+
+app.use(express.static(__dirname));
 
 app.get('/', function (req, res) {
-	res.sendFile( __dirname + "/" + "index.htm" );
+    res.sendFile(__dirname + "/" + "index.htm");
 })
 
-app.get('/getData', function (req, res) {
-   fs.readFile( __dirname + "/data/" + req.query.file, 'ascii', function (err, data) {
-//       console.log( req.query.file );
-		res.type('text/plain');
-       res.send( data );
-   });
+app.get('/getData/vonku', function (req, res) {
+    res.type('application/json');
+    const data = getData('vonku').then(data => res.send(data));
 })
 
-var server = app.listen(8081, function () {
+app.get('/getData/:room', function (req, res) {
+    console.log(req.params.room);
+    res.type('application/json');
+    const data = getData(req.params.room).then(data => res.send(data));
+})
 
-  var host = server.address().address
-  var port = server.address().port
+var server = app.listen(8082, function () {
 
-  console.log("Example app listening at http://%s:%s", host, port)
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("Listening at http://%s:%s", host, port)
 
 })
+
+async function getData(table) {
+    const pool = new Pool({
+        user: 'postgres',
+        host: 'localhost',
+        database: 'postgres',
+        password: 'postgres',
+        port: 5432
+    });
+
+    const client = await pool.connect();
+    console.log('connected ' + table);
+    try {
+        const res = await client.query('SELECT * FROM ' + table + ' where timestamp > now() - \'24 hour\'::interval order by timestamp desc;');
+        return res.rows;
+    } catch (e) {
+        console.log(e);
+    } finally {
+        client.end();
+    }
+}
+
