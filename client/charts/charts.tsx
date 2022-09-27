e383e9c32ddbe1be3755da2b7f9c7d2dcfd5deb8
zaloha <lubo.drobny@gmail.com>
2022-09-27 14:50:34 +0200

    dark mode for a map

--- client/charts/charts.tsx
+++ client/charts/charts.tsx
@@ -227,17 +227,19 @@ const Charts = observer(
       </Row>
       <hr />
       <Row>
-        <MapContainer center={[48.2482, 17.0589]} zoom={13} scrollWheelZoom>
-          <TileLayer
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <Marker position={[48.2482, 17.0589]}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
-        </MapContainer>
+        <div>
+          <MapContainer center={[48.2482, 17.0589]} zoom={13} scrollWheelZoom>
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[48.2482, 17.0589]}>
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+            </Marker>
+          </MapContainer>
+        </div>
       </Row>
     </Container>
   )
