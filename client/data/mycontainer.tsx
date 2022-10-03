b48dcb175d163565ad776f0f0cf5385a5965f569
zaloha <lubo.drobny@gmail.com>
2022-10-03 15:10:28 +0200

    update: packages refactor: mycontainer, myhr
    Signed-off-by: zaloha <lubo.drobny@gmail.com>

---
+++ client/data/mycontainer.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/require-default-props */
+/* eslint-disable no-undef */
+/* eslint-disable import/prefer-default-export */
+import React from "react";
+import { Container } from "react-bootstrap";
+
+type Props = {
+  children?: any;
+};
+
+export function MyContainer({ children }: Props) {
+  return (
+    <Container className="text-center text-light border-secondary bg-very-dark rounded mb-2 py-2">
+      {children}
+    </Container>
+  );
+}
---
+++ client/data/myhr.tsx
@@ -0,0 +1,9 @@
+/* eslint-disable import/prefer-default-export */
+import styled from "styled-components";
+
+export const Myhr = styled.hr`
+  margin-top: 4px;
+  margin-bottom: 1rem;
+  border: 0;
+  border-top: 1px solid rgba(255, 255, 255, 0.9);
+`;
