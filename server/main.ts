c04fdbaae95d6bba68cb65d9b6d713d1a15098e2
Lubo Drobny <ldr@Lubos-MacBook-Pro.local>
2024-10-09 07:51:37 +0200

    fix: syntax

--- server/main.ts
+++ server/main.ts
@@ -68,7 +68,7 @@ app.use(
       frameSrc: [`'self'`, `*.google.com`, `*.chatademian.com`],
       connectSrc: [`'self'`, `*.google.com`],
       imgSrc: [`'self'`, `*.openstreetmap.org`, `unpkg.com`, `data:`],
-      frameAncestors: ['self', `*.chatademian.com`],
+      frameAncestors: [`'self'`, `*.chatademian.com`],
     },
   }),
 );
